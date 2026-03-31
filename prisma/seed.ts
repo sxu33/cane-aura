d7229803736f639ffb140e692a0b6fa9791e97e7
sxu33 <sxu3@academic.rrc.ca>
2026-03-31 16:20:59 -0500

    feat: implement database seeding with sample product data

---
+++ prisma/seed.ts
@@ -0,0 +1,29 @@
+import "dotenv/config";
+import { Pool } from "pg";
+import { PrismaPg } from "@prisma/adapter-pg";
+import { PrismaClient } from "../generated/prisma/client";
+import sampleData from "../lib/sampledata";
+
+const connectionString = `${process.env.DATABASE_URL}`;
+const pool = new Pool({ connectionString });
+const adapter = new PrismaPg(pool);
+const prisma = new PrismaClient({ adapter });
+async function main() {
+  await prisma.product.deleteMany();
+  console.log("data deleted");
+  await prisma.product.createMany({ data: sampleData.products });
+
+  console.log(`${sampleData.products.length} seeded completed successfully`);
+}
+
+main()
+  .then(async () => {
+    await prisma.$disconnect();
+    await pool.end();
+  })
+  .catch(async (e) => {
+    console.error(e);
+    await prisma.$disconnect();
+    await pool.end();
+    process.exit(1);
+  });
