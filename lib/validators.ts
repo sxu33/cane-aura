98939f8dc470e687ca942e63a66e1e133f84ede2
sxu33 <sxu3@academic.rrc.ca>
2026-04-01 23:25:47 -0500

    feat: add Zod product validation schema and derive Product type

---
+++ lib/validators.ts
@@ -0,0 +1,24 @@
+import { z } from "zod";
+import { formatNumberWithDecimals } from "./utils";
+
+const currency = z
+  .string()
+  .refine(
+    (value) => /^\d+(\.\d{2})?$/.test(formatNumberWithDecimals(Number(value))),
+    "Price must be in the right format, e.g., 87.20"
+  );
+
+export const insertProductSchema = z.object({
+  name: z.string().min(3, "Name must be at least 3 characters"),
+  slug: z.string().min(3, "Slug must be at least 3 characters"),
+
+  category: z.string().min(3, "category must be at least 3 characters"),
+
+  description: z.string().min(3, "Description must be at least 3 characters"),
+  images: z.array(z.string()).min(1, "Product must have at least one image"),
+  price: currency,
+  brand: z.string().min(3, "Brand must be at least 3 characters"),
+  countInStock: z.coerce.number(),
+  isFeatured: z.boolean(),
+  banner: z.string().nullable(),
+});
